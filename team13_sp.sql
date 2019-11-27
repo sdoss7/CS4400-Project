e66f776ebb8278b3354f14642a770f973d50b9d5
bayne <nicholasgrubb@gmail.com>
2019-11-27 14:15:35 -0500

    yeet

--- team13_sp.sql
+++ team13_sp.sql
@@ -1,3 +1,81 @@
+DROP PROCEDURE IF EXISTS user_login;
+DELIMITER $$
+CREATE PROCEDURE `user_login`(IN i_username VARCHAR(50), IN i_password VARCHAR(50))
+BEGIN
+	DROP TABLE IF EXISTS UserLogin;
+    CREATE TABLE UserLogin(username VARCHAR(50), status VARCHAR(50), isCustomer INT, isAdmin INT, isManager INT);
+		SELECT username, status FROM user WHERE username = i_username AND password = MD5(i_password);
+		SELECT COUNT(*) FROM customer WHERE username in(
+		SELECT username FROM user WHERE username = i_username AND password = MD5(i_password)
+		);
+		SELECT COUNT(*) FROM admin WHERE username in(
+		SELECT username FROM user WHERE username = i_username AND password = MD5(i_password)
+		);
+		SELECT COUNT(*) FROM manager WHERE username in(
+		SELECT username FROM user WHERE username = i_username AND password = MD5(i_password)
+		);
+
+END
+$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS customer_only_register;
+DELIMITER $$
+CREATE PROCEDURE `customer_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
+BEGIN
+    INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
+    INSERT INTO customer (username) VALUES (i_username);
+END
+$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS customer_add_creditcard;
+DELIMITER $$ 
+CREATE PROCEDURE `customer_add_creditcard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
+BEGIN 
+    INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
+END 
+$$
+DELIMITER ; 
+
+DROP PROCEDURE IF EXISTS manager_only_register;
+DELIMITER $$
+CREATE PROCEDURE `manager_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), i_comName VARCHAR(50),
+    i_empStreet VARCHAR(50), i_empCity VARCHAR(50), i_empState VARCHAR(50), i_empZipcode VARCHAR(50))
+BEGIN    
+    INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
+    INSERT INTO manager (username, comName, empStreet, empCity, empState, empZipcode) 
+		VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
+    INSERT INTO employee (username) VALUES (i_username);
+END
+$$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS manager_customer_register;
+DELIMITER $$
+CREATE PROCEDURE `manager_customer_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), i_comName VARCHAR(50),
+    i_empStreet VARCHAR(50), i_empCity VARCHAR(50), i_empState VARCHAR(50), i_empZipcode VARCHAR(50))
+BEGIN    
+    INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
+    INSERT INTO manager (username, comName, empStreet, empCity, empState, empZipcode)
+		VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
+    INSERT INTO customer (username) VALUES (i_username);
+    INSERT INTO employee (username) VALUES (i_username);
+END
+$$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS manager_customer_add_creditcard;
+DELIMITER $$ 
+CREATE PROCEDURE `manager_customer_add_creditcard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
+BEGIN 
+    INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
+END 
+$$
+DELIMITER ;
+
 DROP PROCEDURE IF EXISTS user_register;
 DELIMITER $$
 CREATE PROCEDURE `user_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
@@ -52,7 +130,7 @@ DROP PROCEDURE IF EXISTS admin_approve_user;
 DELIMITER $$
 CREATE PROCEDURE `admin_approve_user` (IN i_username VARCHAR(50))
 BEGIN
-	UPDATE users SET status = 'Approved' WHERE username = i_username;
+	UPDATE user SET status = 'Approved' WHERE username = i_username;
 END$$
 DELIMITER ;
 
@@ -60,11 +138,11 @@ DROP PROCEDURE IF EXISTS admin_decline_user;
 DELIMITER $$
 CREATE PROCEDURE `admin_decline_user` (IN i_username VARCHAR(50))
 BEGIN
-	UPDATE users SET status = 'Declined' WHERE username = i_username;
+	UPDATE user SET status = 'Declined' WHERE username = i_username;
 END$$
 DELIMITER ;
 
-DROP PROCEDURE IF EXISTS admin_create_theater
+DROP PROCEDURE IF EXISTS admin_create_theater;
 DELIMITER $$
 CREATE PROCEDURE `admin_create_theater` (IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_thCity VARCHAR(50), IN i_thStreet VARCHAR(50), IN i_thState CHAR(2), 
 IN i_thZipcode CHAR(5), IN i_capacity INT, IN i_managerUsername VARCHAR(50))
