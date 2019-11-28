451bf6fca9dacd763f3b58d3c65df0799c59355e
bayne <nicholasgrubb@gmail.com>
2019-11-28 18:05:35 -0500

    all html skeletons done now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,8 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('layout.html')
-
-
 
 if __name__ == '__main__':
     app.run()
